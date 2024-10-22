a71f6c6ddfa669459b037826a61904d83458199c
Josemar amilton silva de almeida <amilton_cg12@hotmail.com>
2024-10-22 20:51:04 -0300

    alteração do numero de escolha

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -13,7 +13,7 @@ function exibirNaTela(tag, texto) {
 // Função para exibir texto na tela
 function exibirMsgInicial() {
     exibirNaTela("h1", "Jogo do número secreto");
-    exibirNaTela("p", "Escolha um número entre 1 e 10");
+    exibirNaTela("p", `Escolha um número entre 1 e ${numeroLimite}` );
 }
 
 exibirMsgInicial()
